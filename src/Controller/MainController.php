a7e58ecd50aded76ebb0113132033fbaf2098690
karimaweb <sespoir362@gmail.com>
2025-02-10 16:20:06 +0100

    remplir la bdd

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controller;
+use App\Repository\ProduitRepository; // Assurez-vous que cette ligne est présente
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,8 +10,12 @@ use Symfony\Component\Routing\Attribute\Route;
 final class MainController extends AbstractController
 {
     #[Route('/main', name: 'app_main')]
-    public function index(): Response
-    {
-        return $this->render('main/index.html.twig');
-    }
+    public function index(ProduitRepository $produitRepository): Response
+{
+    $produits = $produitRepository->findAll();
+
+    return $this->render('main/index.html.twig', [
+        'produits' => $produits,
+    ]);
+}
 }
